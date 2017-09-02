259b9b2c5df261e5534a73719902ac6622e5ce93
Vatekeh Corlon <vatekehcorlon@Vatekehs-MBP.fios-router.home>
2017-09-02 16:51:55 -0400

    one more before bed

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -14,9 +14,21 @@ angular.module('app').controller('mainCtrl', function($scope, mainServ){
     }
 
     $scope.killChip = (chip) => {
-        $scope.chips.filter(val => {
-            if(val.text){} 
-        })
+        console.log(chip)
+        console.log($scope.chips);
+
+        for(let i = 0; i < $scope.chips.length; i++){
+
+            if(i == 0 && $scope.chips[i].text == chip.text){
+                $scope.chips.reverse();
+                $scope.chips.pop();
+                $scope.chips.reverse();
+            }
+            else if($scope.chips[i].text == chip.text){
+                $scope.chips.splice(i, i);
+            }
+        }
+        console.log($scope.chips);
     }
 
    
